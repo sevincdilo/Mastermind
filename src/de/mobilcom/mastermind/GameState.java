e90d539afa0a0cc3db11206d3944f8a42224477d
Dilayla Sevinc <Dilayla.Sevinc@md.de>
2020-09-22 08:00:16 +0200

    built

---
+++ src/de/mobilcom/mastermind/GameState.java
@@ -0,0 +1,9 @@
+package de.mobilcom.mastermind;
+
+public class GameState {
+    public static void main(String[] args) {
+        int colors;
+        int fields;
+        System.out.println("hey");
+    }
+}
